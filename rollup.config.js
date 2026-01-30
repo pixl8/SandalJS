c35a83265ec00238b816ee928083200c4d936998
Alex Skinner <alex@pixl8.co.uk>
2026-01-30 10:34:44 +0000

    Add GitHub Actions release workflow

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,48 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
+import { readFileSync } from 'fs';
 
-export default {
-  input: 'src/sandal.js',
-  output: [
-    {
-      file: 'dist/sandal.js',
-      format: 'umd',
-      name: 'Sandal',
-      sourcemap: true
-    },
-    {
-      file: 'dist/sandal.min.js',
-      format: 'umd',
-      name: 'Sandal',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    {
-      file: 'dist/sandal.esm.js',
+// Read version from package.json
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+const version = pkg.version;
+
+// Banner with version number
+const banner = `/*!
+ * SandalJS v${version}
+ * Copyright (c) ${new Date().getFullYear()} Ready Intelligence
+ * Released under the MIT License
+ */`;
+
+export default [
+  {
+    input: 'src/sandal.js',
+    output: [
+      {
+        file: `dist/sandal-${version}.js`,
+        format: 'umd',
+        name: 'Sandal',
+        banner,
+        sourcemap: true
+      },
+      {
+        file: `dist/sandal-${version}.min.js`,
+        format: 'umd',
+        name: 'Sandal',
+        banner,
+        sourcemap: true,
+        plugins: [terser({ format: { comments: /^!/ } })]
+      }
+    ],
+    plugins: [nodeResolve()]
+  },
+  {
+    input: 'src/sandal.js',
+    output: {
+      file: `dist/sandal-${version}.esm.js`,
       format: 'es',
+      banner,
       sourcemap: true
-    }
-  ],
-  plugins: [nodeResolve()]
-};
+    },
+    plugins: [nodeResolve()]
+  }
+];
